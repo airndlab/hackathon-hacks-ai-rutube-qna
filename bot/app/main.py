9357b3f5948173d744ece3542c1ecfaf282082d7
Grig Alex <alexengrigdev@gmail.com>
2024-09-28 23:09:38 +0300

    Refactor bot messages

--- bot/app/main.py
+++ bot/app/main.py
@@ -11,7 +11,7 @@ from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 
 from qna import get_answer, Answer, like_answer, dislike_answer
-from settings import init_db, set_pipeline, get_pipeline_or_default, get_verbose_or_default, pipelines, set_verbose
+from settings import init_db, get_pipeline_or_default, get_verbose_or_default, pipelines, set_verbose, set_pipeline
 
 logging.basicConfig(
     level=logging.INFO,
@@ -23,68 +23,71 @@ logging.basicConfig(
 
 BOT_MESSAGES_FILE_PATH = os.getenv('BOT_MESSAGES_FILE_PATH')
 with open(BOT_MESSAGES_FILE_PATH, 'r', encoding='utf-8') as file:
-    messages = yaml.safe_load(file)
+    bot_messages = yaml.safe_load(file)
 
 dp = Dispatcher()
 
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    await message.reply(messages['start'])
-
-
-async def get_pipelines_text():
-    pipelines_list = "\n\n".join([f"- /pipeline {name}: {description}" for name, description in pipelines.items()])
-    response = f"Доступные пайплайны:\n\n{pipelines_list}"
-    return response
+    await message.reply(bot_messages['start'])
 
 
 @dp.message(Command('pipelines'))
 async def command_pipelines_handler(message: Message):
     current_pipeline = await get_pipeline_or_default(message.chat.id)
-    pipelines_text = await get_pipelines_text()
-    text = f'Текущий пайплайн: {current_pipeline}\n\n{pipelines_text}'
-    await message.reply(text)
+    text = bot_messages['pipelines'].format(pipeline=pipelines[current_pipeline])
+    markup = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text=name, callback_data=f'pipeline:{key}')]
+        for key, name in pipelines.items()
+    ])
+    await message.reply(text, reply_markup=markup)
 
 
-@dp.message(Command('pipeline'))
-async def command_pipeline_handler(message: Message):
-    args = message.text.split()
-    if len(args) != 2:
-        text = f"Пожалуйста, укажите название пайплайна."
-        await message.reply(text)
-        return
-    pipeline_name = args[1]
-    if pipeline_name in pipelines:
-        await set_pipeline(message.chat.id, pipeline_name)
-        response = f"Выбран пайплайн {pipeline_name}: {pipelines[pipeline_name]}"
-    else:
-        response = f'Пайплайн не найден. {get_pipelines_text()}'
-    await message.answer(response)
+@dp.callback_query(lambda query: query.data.startswith('pipeline:'))
+async def pipeline_handler(query: CallbackQuery):
+    pipeline_name = query.data.split(':')[1]
+    await set_pipeline(query.message.chat.id, pipeline_name)
+    await query.answer(bot_messages['pipeline'].format(pipeline=pipelines[pipeline_name]))
 
 
-@dp.message(Command('enable_verbose'))
-async def command_enable_verbose_handler(message: Message):
-    await set_verbose(message.chat.id, True)
-    await message.answer('Подробный режим: Включен')
+@dp.message(Command('verbose'))
+async def command_verbose_handler(message: Message):
+    verbose = await get_verbose_or_default(message.chat.id)
+    text = bot_messages['verbose'].format(status=get_verbose_status(verbose))
+    markup = InlineKeyboardMarkup(inline_keyboard=[[
+        InlineKeyboardButton(text="✅", callback_data=f'verbose:true'),
+        InlineKeyboardButton(text="❌", callback_data=f'verbose:false'),
+    ]])
+    await message.reply(text, reply_markup=markup)
 
 
-@dp.message(Command('disable_verbose'))
-async def command_disable_verbose_handler(message: Message):
-    await set_verbose(message.chat.id, False)
-    await message.answer('Подробный режим: Выключен')
+def get_verbose_status(verbose):
+    verbose_status = bot_messages['verbose-enabled'] if verbose else bot_messages['verbose-disabled']
+    return verbose_status
+
+
+@dp.callback_query(lambda query: query.data.startswith('verbose:'))
+async def pipeline_handler(query: CallbackQuery):
+    verbose_value = query.data.split(':')[1]
+    verbose = verbose_value.lower() == 'true'
+    await set_verbose(query.message.chat.id, verbose)
+    verbose_status = get_verbose_status(verbose)
+    await query.answer(bot_messages['verbose-select'].format(status=verbose_status))
 
 
 def get_answer_text(response: Answer, verbose: bool) -> str:
+    assurance_text = bot_messages['answer-confident'] if True else bot_messages['answer-doubtful']
     other_text = ''
     if verbose:
         other_inline = response.get_other_inline()
         if other_inline:
             other_text = other_inline
-    text = messages['answer'].format(
+    text = bot_messages['answer'].format(
         answer=response.answer,
         class_1=response.class_1,
         class_2=response.class_2,
+        assurance=assurance_text,
         other=other_text
     )
     return text
@@ -104,20 +107,25 @@ async def question_handler(message: Message) -> None:
         ]])
         await message.reply(text, reply_markup=markup)
     except Exception as e:
-        await message.reply(f'Ой! Произошла непредвиденная ошибка, нам очень жаль...\n\n{e}')
+        verbose = await get_verbose_or_default(message.chat.id)
+        exception = ''
+        if verbose:
+            exception = str(e)
+        error_text = bot_messages['error'].format(exception=exception)
+        await message.reply(error_text)
         raise e
 
 
-@dp.callback_query(lambda query: query.data.startswith('like'))
+@dp.callback_query(lambda query: query.data.startswith('like:'))
 async def like_handler(query: CallbackQuery):
     await like_answer(query.data.split(':')[1])
-    await query.answer(messages['like'])
+    await query.answer(bot_messages['like'])
 
 
-@dp.callback_query(lambda query: query.data.startswith('dislike'))
+@dp.callback_query(lambda query: query.data.startswith('dislike:'))
 async def like_handler(query: CallbackQuery):
     await dislike_answer(query.data.split(':')[1])
-    await query.answer(messages['dislike'])
+    await query.answer(bot_messages['dislike'])
 
 
 async def main() -> None:
